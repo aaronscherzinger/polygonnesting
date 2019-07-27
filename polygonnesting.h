24bf0c35041b630575b2a9d9afdbd0791401b608
Aaron Scherzinger <aaron.scherzinger@googlemail.com>
2019-07-27 19:22:01 +0200

    work on polygon nesting algorithm

--- polygonnesting.h
+++ polygonnesting.h
@@ -237,6 +237,7 @@ void PolygonNesting(const PolygonSet& polygonSet)
             else
             {
                 // at this point, the two endpoints connect two subchains of the same polygon
+                // we always put a right endpoint before a left endpoint
                 return vertA > vertB;
             }
         });
@@ -282,19 +283,280 @@ void PolygonNesting(const PolygonSet& polygonSet)
     }
 
     // we remember the current edge where the sweep line stands by its start vertex
-    // (INVALID_INDEX means that the subchain has not been encountered yet)
+    // (the initial INVALID_INDEX means that a subchains has not been visited yet)
     subchains[s1].currentEdge = 0;
     subchains[s2].currentEdge = 0;
 
     // #TODO: we already know that the polygon of subchains s1, s2 does not have a parent as it has the leftmost vertex
     // #TODO: create a graph for the parent relationships? how to represent it in the most efficient / useful manner?
 
-    // setp 4: perform plane sweep from left to right
+    // step 4: perform plane sweep from left to right
 
-    // #TODO
     for (size_t sweepLineIndex = 2; sweepLineIndex < sortedEndpoints.size(); ++sweepLineIndex)
     {
+        // get the next subchain endpoint v_i
+        std::pair<size_t, size_t>& currentEndpoint = sortedEndpoints[sweepLineIndex];
 
+        // curent sweep line x-coordinate
+        float sweepLineCoord = (*(polygonSet[subchains[currentEndpoint.first].polygon]))[subchains[currentEndpoint.first].vertices[currentEndpoint.second]].y;
+
+        // check if we are at the right endpoint of a subchain
+        bool currentSubchainVisited = (currentEndpoint.second > 0);
+        // check if the other subchain connected to this vertex has already been visited
+        size_t otherSubchainConnectedToCurrentEndpoint = INVALID_INDEX;
+        bool otherSubchainVisited = false;
+        bool currentEndpointIsLastEndpoint = (sweepLineIndex == sortedEndpoints.size() - 1);
+        if (currentEndpointIsLastEndpoint)
+        {
+            // if we are at the last vertex in the queue, we obviously have encountered the other subchain
+            otherSubchainVisited = true;
+        }
+        else if (currentSubchainVisited)
+        {
+            // since we are at the endpoint of a subchain that has not been encountered yet,
+            //  another subchain which is connected to this endpoint must come directly next as its x-coordinate and y-coordinate is the same
+            //  # TODO: confirm!!!
+            std::pair<size_t, size_t>& nextEndpoint = sortedEndpoints[sweepLineIndex + 1];
+            otherSubchainConnectedToCurrentEndpoint = nextEndpoint.first;
+
+            assert(subchains[currentEndpoint.first].polygon == subchains[nextEndpoint.first].polygon);
+
+            otherSubchainVisited = (nextEndpoint.second > 0);
+        }
+
+        // if both subchains connected to v_i have already been visited, delete them from the list of ordered subchains and continue
+        if (currentSubchainVisited && otherSubchainVisited)
+        {
+            if (currentEndpointIsLastEndpoint)
+            {
+                assert(orderedSubchains.size() == 2);
+                orderedSubchains.clear();
+            }
+            else
+            {
+                assert(otherSubchainConnectedToCurrentEndpoint != INVALID_INDEX);
+                // find the two subchains and remove them from the list of ordered subchains
+                auto firstIterator = std::lower_bound(orderedSubchains.begin(), orderedSubchains.end(), currentEndpoint.first, 
+                        [&](const size_t& a, const size_t& b)
+                        {
+                            auto& polygonA = *(polygonSet[subchains[a].polygon]);
+                            auto& polygonB = *(polygonSet[subchains[b].polygon]);
+
+                            // we need to compare the subchains regarding their y-coordinate of the intersection with the sweep line
+                            while(polygonA[subchains[a].vertices[subchains[a].currentEdge]].x < sweepLineCoord)
+                            {
+                                subchains[a].currentEdge++;
+                            }
+                            assert(subchains[a].currentEdge < subchains[a].vertices.size());
+                            // note: vertices [currentEdge - 1] and [currentEdge] correspond to the edge intersecting the sweep line
+                            float yCoordA = polygonA[subchains[a].vertices[subchains[a].currentEdge]].y;
+                            if (polygonA[subchains[a].vertices[subchains[a].currentEdge]].x > sweepLineCoord)
+                            {
+                                assert(subchains[a].currentEdge > 0);
+                                // sweep line intersects the edge and not the vertex
+                                // we can compute the intersection by linear interpolation
+                                float x1 = polygonA[subchains[a].vertices[subchains[a].currentEdge - 1]].x;
+                                float y1 = polygonA[subchains[a].vertices[subchains[a].currentEdge - 1]].y;
+
+                                float x2 = polygonA[subchains[a].vertices[subchains[a].currentEdge]].x;
+                                float y2 = polygonA[subchains[a].vertices[subchains[a].currentEdge]].y;
+
+                                assert(x1 != x2);
+                                assert(sweepLineCoord >= x1);
+                                assert(sweepLineCoord <= x2);
+
+                                float ratio = (sweepLineCoord - x1) / (x2 - x1);
+                                yCoordA = (1.f - ratio) * y1 + ratio * y2;
+                            }
+
+                            while(polygonB[subchains[b].vertices[subchains[b].currentEdge]].x < sweepLineCoord)
+                            {
+                                subchains[b].currentEdge++;
+                            }
+                            assert(subchains[b].currentEdge < subchains[b].vertices.size());
+                            // note: vertices [currentEdge - 1] and [currentEdge] correspond to the edge intersecting the sweep line
+                            float yCoordB = polygonB[subchains[b].vertices[subchains[b].currentEdge]].y;
+                            if (polygonB[subchains[b].vertices[subchains[b].currentEdge]].x > sweepLineCoord)
+                            {
+                                assert(subchains[b].currentEdge > 0);
+                                // sweep line intersects the edge and not the vertex
+                                // we can compute the intersection by linear interpolation
+                                float x1 = polygonB[subchains[b].vertices[subchains[b].currentEdge - 1]].x;
+                                float y1 = polygonB[subchains[b].vertices[subchains[b].currentEdge - 1]].y;
+
+                                float x2 = polygonB[subchains[b].vertices[subchains[b].currentEdge]].x;
+                                float y2 = polygonB[subchains[b].vertices[subchains[b].currentEdge]].y;
+
+                                assert(x1 != x2);
+                                assert(sweepLineCoord >= x1);
+                                assert(sweepLineCoord <= x2);
+
+                                float ratio = (sweepLineCoord - x1) / (x2 - x1);
+                                yCoordB = (1.f - ratio) * y1 + ratio * y2;
+                            }
+
+                            if (yCoordA == yCoordB)
+                            {
+                                // if this happens, a shared endpoint of two subchains has been met
+                                assert(polygonA[subchains[a].vertices[subchains[a].currentEdge]].x == sweepLineCoord);
+                                assert(polygonB[subchains[b].vertices[subchains[b].currentEdge]].x == sweepLineCoord);
+                                assert(subchains[a].polygon == subchains[b].polygon);
+
+                                // this means that one of the following cases has happened:
+                                // 1. we encounter the endpoint of one subchain that is the starting point of another one
+                                // 2. we encounter the startpoint of two subchains
+                                // 3. we encounter the endpoint of two subchains
+
+                                // case 1: we always put the endpoint of one chain before the starting point of the next as this does not affect the y-order
+                                if (subchains[a].currentEdge == 0 && subchains[b].currentEdge > 0)
+                                {
+                                    return true;
+                                }
+                                else if (subchains[b].currentEdge == 0 && subchains[a].currentEdge > 0)
+                                {
+                                    return false;
+                                } 
+                                else if (subchains[a].currentEdge == 0 && subchains[b].currentEdge == 0)
+                                {
+                                    // case 2: we put the subchain with the higher y-coordinate in the next vertex first
+                                    assert(subchains[a].vertices.size() > 1);
+                                    assert(subchains[b].vertices.size() > 1);
+
+                                    return (polygonA[subchains[a].vertices[1]].y > polygonB[subchains[b].vertices[1]].y);
+                                }
+                                else 
+                                {
+                                    // case 3: we put the subchains with the higher y-coordinate in the preceding vertex first
+                                    return (polygonA[subchains[a].vertices[subchains[a].currentEdge - 1]].y > polygonB[subchains[b].vertices[subchains[b].currentEdge - 1]].y);
+                                }
+                            }
+                            else
+                            {
+                                return (yCoordA > yCoordB);
+                            }
+                        }
+                );
+
+                assert(firstIterator != orderedSubchains.end());
+
+                orderedSubchains.erase(firstIterator);
+                
+                auto secondIterator = std::lower_bound(orderedSubchains.begin(), orderedSubchains.end(), otherSubchainConnectedToCurrentEndpoint, 
+                        [&](const size_t& a, const size_t& b)
+                        {
+                            auto& polygonA = *(polygonSet[subchains[a].polygon]);
+                            auto& polygonB = *(polygonSet[subchains[b].polygon]);
+
+                            // we need to compare the subchains regarding their y-coordinate of the intersection with the sweep line
+                            while(polygonA[subchains[a].vertices[subchains[a].currentEdge]].x < sweepLineCoord)
+                            {
+                                subchains[a].currentEdge++;
+                            }
+                            assert(subchains[a].currentEdge < subchains[a].vertices.size());
+                            // note: vertices [currentEdge - 1] and [currentEdge] correspond to the edge intersecting the sweep line
+                            float yCoordA = polygonA[subchains[a].vertices[subchains[a].currentEdge]].y;
+                            if (polygonA[subchains[a].vertices[subchains[a].currentEdge]].x > sweepLineCoord)
+                            {
+                                assert(subchains[a].currentEdge > 0);
+                                // sweep line intersects the edge and not the vertex
+                                // we can compute the intersection by linear interpolation
+                                float x1 = polygonA[subchains[a].vertices[subchains[a].currentEdge - 1]].x;
+                                float y1 = polygonA[subchains[a].vertices[subchains[a].currentEdge - 1]].y;
+
+                                float x2 = polygonA[subchains[a].vertices[subchains[a].currentEdge]].x;
+                                float y2 = polygonA[subchains[a].vertices[subchains[a].currentEdge]].y;
+
+                                assert(x1 != x2);
+                                assert(sweepLineCoord >= x1);
+                                assert(sweepLineCoord <= x2);
+
+                                float ratio = (sweepLineCoord - x1) / (x2 - x1);
+                                yCoordA = (1.f - ratio) * y1 + ratio * y2;
+                            }
+
+                            while(polygonB[subchains[b].vertices[subchains[b].currentEdge]].x < sweepLineCoord)
+                            {
+                                subchains[b].currentEdge++;
+                            }
+                            assert(subchains[b].currentEdge < subchains[b].vertices.size());
+                            // note: vertices [currentEdge - 1] and [currentEdge] correspond to the edge intersecting the sweep line
+                            float yCoordB = polygonB[subchains[b].vertices[subchains[b].currentEdge]].y;
+                            if (polygonB[subchains[b].vertices[subchains[b].currentEdge]].x > sweepLineCoord)
+                            {
+                                assert(subchains[b].currentEdge > 0);
+                                // sweep line intersects the edge and not the vertex
+                                // we can compute the intersection by linear interpolation
+                                float x1 = polygonB[subchains[b].vertices[subchains[b].currentEdge - 1]].x;
+                                float y1 = polygonB[subchains[b].vertices[subchains[b].currentEdge - 1]].y;
+
+                                float x2 = polygonB[subchains[b].vertices[subchains[b].currentEdge]].x;
+                                float y2 = polygonB[subchains[b].vertices[subchains[b].currentEdge]].y;
+
+                                assert(x1 != x2);
+                                assert(sweepLineCoord >= x1);
+                                assert(sweepLineCoord <= x2);
+
+                                float ratio = (sweepLineCoord - x1) / (x2 - x1);
+                                yCoordB = (1.f - ratio) * y1 + ratio * y2;
+                            }
+
+                            if (yCoordA == yCoordB)
+                            {
+                                // if this happens, a shared endpoint of two subchains has been met
+                                assert(polygonA[subchains[a].vertices[subchains[a].currentEdge]].x == sweepLineCoord);
+                                assert(polygonB[subchains[b].vertices[subchains[b].currentEdge]].x == sweepLineCoord);
+                                assert(subchains[a].polygon == subchains[b].polygon);
+
+                                // this means that one of the following cases has happened:
+                                // 1. we encounter the endpoint of one subchain that is the starting point of another one
+                                // 2. we encounter the startpoint of two subchains
+                                // 3. we encounter the endpoint of two subchains
+
+                                // case 1: we always put the endpoint of one chain before the starting point of the next as this does not affect the y-order
+                                if (subchains[a].currentEdge == 0 && subchains[b].currentEdge > 0)
+                                {
+                                    return true;
+                                }
+                                else if (subchains[b].currentEdge == 0 && subchains[a].currentEdge > 0)
+                                {
+                                    return false;
+                                } 
+                                else if (subchains[a].currentEdge == 0 && subchains[b].currentEdge == 0)
+                                {
+                                    // case 2: we put the subchain with the higher y-coordinate in the next vertex first
+                                    assert(subchains[a].vertices.size() > 1);
+                                    assert(subchains[b].vertices.size() > 1);
+
+                                    return (polygonA[subchains[a].vertices[1]].y > polygonB[subchains[b].vertices[1]].y);
+                                }
+                                else 
+                                {
+                                    // case 3: we put the subchains with the higher y-coordinate in the preceding vertex first
+                                    return (polygonA[subchains[a].vertices[subchains[a].currentEdge - 1]].y > polygonB[subchains[b].vertices[subchains[b].currentEdge - 1]].y);
+                                }
+                            }
+                            else
+                            {
+                                return (yCoordA > yCoordB);
+                            }
+                        }
+                );
+
+                assert(secondIterator != orderedSubchains.end());
+
+                orderedSubchains.erase(secondIterator);
+
+                // since we already removed the successor subchain, we need to increment the index 
+                sweepLineIndex++;
+            }
+
+            continue;
+        }
+
+        // #TODO: step 4(a) and following
+
+
+        
     }
 }
 
